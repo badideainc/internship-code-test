d9969b1badd0f12dc2bba9ec1d8d84c2ee8ac697
badideainc <untitledprogrammer@gmail.com>
2026-04-07 18:59:19 +0100

    Fix incorrect randomEmptyCellIndex

--- main.js
+++ main.js
@@ -25,7 +25,8 @@ function simulateComputerTurn() {
 	// find the empty cells, pick a random one and then place a marker
 	const cells = getCells()
 	const emptyCells = cells.filter((cell) => cell.textContent === '')
-	const randomEmptyCellIndex = Math.ceil(Math.random() * emptyCells.length)
+	const randomEmptyCellIndex = Math.ceil(Math.random() * emptyCells.length - 1)
+
 	const randomEmptyCell = emptyCells[randomEmptyCellIndex]
 	randomEmptyCell.textContent = computerMarker
 }
